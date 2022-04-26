6c08e11287adc1a32c9dbaf47ad7e08287012f5b
Ilya Shumilin <shumilin@skbkontur.ru>
2022-04-27 00:52:46 +0500

    Add echo

---
+++ BackendEcho/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendEcho
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var listener = new TcpListener(new IPEndPoint(IPAddress.Loopback, 5051));
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Wait connection");
+                using var client = await listener.AcceptTcpClientAsync();
+                while (true)
+                {
+                    Console.WriteLine("Connection accepted");
+                    var readPipe = PipeReader.Create(client.GetStream());
+
+                    var result = await readPipe.ReadAsync();
+
+                    var buffer = result.Buffer;
+
+                    foreach (var segment in buffer)
+                    {
+                        Console.WriteLine(Encoding.UTF8.GetString(segment.Span));
+                    }
+
+                    if (result.IsCompleted)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ClientEcho/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientEcho
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Connecting to port 5019");
+
+            await clientSocket.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 5027));
+
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var message = Encoding.UTF8.GetBytes(line);
+                await clientSocket.SendAsync(new ArraySegment<byte>(message), SocketFlags.None);
+            }
+        }
+    }
+}
